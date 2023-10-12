431f0afee117dacd86b077697bc3e8a24a41312f
Marcus Kim <marcuskim1989@gmail.com>
2023-10-11 22:20:18 -0700

    Initial commit

--- AwesomeProject/main.m
+++ AwesomeProject/main.m
@@ -2,9 +2,10 @@
 
 #import "AppDelegate.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
+  
+  // I don't know why this looks like C++
 }
